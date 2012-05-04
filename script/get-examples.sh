303cca1f4fad3142e0a19455edb196fefbbf02ad
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-04 15:59:20 -0400

    fixed literal string passed into cut

--- script/get-examples.sh
+++ script/get-examples.sh
@@ -2,7 +2,7 @@
 
 cat templates.txt | while read line; do
   echo "processing $line"
-  wd=`cut -d- -f1 $line`
+  wd=`cut -d- -f1 < echo "$line"`
   grep "$wd" newscomm-en/newscomm-en.txt > example-sents/$line
 done
 
