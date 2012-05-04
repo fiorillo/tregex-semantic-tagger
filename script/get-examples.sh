16f181e624c4d39de7ef2d21abd3385de3d2bbdf
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-04 15:56:54 -0400

    fixed some more sh errors (bah)

--- script/get-examples.sh
+++ script/get-examples.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cat templates.txt | while read line; do
-  echo 'processing $line'
-  $wd=`'$line' > cut -d- -f1`
-  grep '$wd' newscomm-en/newscomm-en.txt > example-sents/$line
+  echo "processing $line"
+  $wd=`"$line" > cut -d- -f1`
+  grep "$wd" newscomm-en/newscomm-en.txt > example-sents/$line
 done
 
 exit 0
