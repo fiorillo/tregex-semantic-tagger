cb9029bcee502c6d7df2db9d1fec5264cdd1ef47
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-06 18:06:46 -0400

    changed to relative pathnames

--- script/parse-examples.sh
+++ script/parse-examples.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ls example-sents/*.head | while read file; do
-  touch example-sents/$file.parsed
-  stanford-parser-2012-01-06/lexparser.sh example-sents/$file > example-sents/$file.parsed
+  touch $file.parsed
+  stanford-parser-2012-01-06/lexparser.sh $file > $file.parsed
 done
 
 exit 0
