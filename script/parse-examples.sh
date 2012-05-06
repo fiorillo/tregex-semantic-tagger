91ce9acaf550551088417f715fb54a48c78fba4b
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-06 18:05:56 -0400

    create parse file before writing to it

--- script/parse-examples.sh
+++ script/parse-examples.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ls example-sents/*.head | while read file; do
+  touch example-sents/$file.parsed
   stanford-parser-2012-01-06/lexparser.sh example-sents/$file > example-sents/$file.parsed
 done
 
