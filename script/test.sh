977de0d3c0abbd5b3dd167bb7eb66c2469869c21
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 16:59:27 -0400

    generalized test script for testing fixed instantiated templates files

---
+++ script/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pattern_name=$1
+
+root=/Users/matthewfiorillo/Documents/school/tsurgeon
+cd $root/stanford-tregex-2012-03-09
+./tsurgeon.sh -treeFile $root/example-sents/$pattern_name.txt.head.parsed $root/modality-tagger/modality-patterns/fixed-instantiated-templates/$pattern_name.txt > $root/output/$pattern_name.tree
+
+exit 0
