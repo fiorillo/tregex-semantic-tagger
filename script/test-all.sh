6c8b13cfd3970f177e02aa58e420b129ee32c731
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 15:29:10 -0400

    cd'd into tregex directory so the java classpath would be right

--- script/test-all.sh
+++ script/test-all.sh
@@ -2,7 +2,8 @@
 
 ls example-sents/*.parsed | while read file; do
   root=`echo $file | cut -d. -f1 | cut -d/ -f2`
-  stanford-tregex-2012-03-09/tsurgeon.sh -treeFile $file modality-tagger/modality-patterns/instantiated-templates/$root.txt > output/$root.tree
+  cd stanford-tregex-2012-03-09
+  ./tsurgeon.sh -treeFile $file ../modality-tagger/modality-patterns/instantiated-templates/$root.txt > ../output/$root.tree
 done
 
 exit 0
