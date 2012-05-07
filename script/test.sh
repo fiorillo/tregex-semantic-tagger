b83b2d4a55f256b922da6dd70877b6414e707495
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 17:03:10 -0400

    made fixed-patterns directory for pattern testing (so that test.sh knows where to look for them)

--- script/test.sh
+++ script/test.sh
@@ -4,6 +4,6 @@ pattern_name=$1
 
 root=/Users/matthewfiorillo/Documents/school/tsurgeon
 cd $root/stanford-tregex-2012-03-09
-./tsurgeon.sh -treeFile $root/example-sents/$pattern_name.txt.head.parsed $root/modality-tagger/modality-patterns/fixed-instantiated-templates/$pattern_name.txt > $root/output/$pattern_name.tree
+./tsurgeon.sh -treeFile $root/example-sents/$pattern_name.txt.head.parsed $root/modality-tagger/modality-patterns/fixed-patterns/$pattern_name.txt > $root/output/$pattern_name.tree
 
 exit 0
