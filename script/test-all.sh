c188b749ceddff787eb1b9d21cce045ebf46418f
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 15:11:24 -0400

    put quotes around filename so the whole thing won't be read into cut

--- script/test-all.sh
+++ script/test-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ls example-sents/*.parsed | while read file; do
-  root=`cut -d. -f1 $file`
+  root=`cut -d. -f1 '$file'`
   echo $root
   #stanford-tregex-2012-03-09/tsurgeon.sh -treeFile $file 
 done
