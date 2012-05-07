3259c3eb2716942974aaad1d3289478141144561
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 15:12:45 -0400

    used echos instead to get the filename cutting working properly

--- script/test-all.sh
+++ script/test-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ls example-sents/*.parsed | while read file; do
-  root=`cut -d. -f1 '$file'`
+  root=`echo $file | cut -d. -f1 | cut -d/ -f2`
   echo $root
   #stanford-tregex-2012-03-09/tsurgeon.sh -treeFile $file 
 done
