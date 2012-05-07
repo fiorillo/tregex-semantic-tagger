de469762ed7961525861d5adc0de8a915d5bf2c3
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 15:36:35 -0400

    made all path names absolute

--- script/test-all.sh
+++ script/test-all.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+root=/Users/matthewfiorillo/Documents/school/tsurgeon
+cd $root
 ls example-sents/*.parsed | while read file; do
-  root=`echo $file | cut -d. -f1 | cut -d/ -f2`
-  cd stanford-tregex-2012-03-09
-  ./tsurgeon.sh -treeFile $file ../modality-tagger/modality-patterns/instantiated-templates/$root.txt > ../output/$root.tree
+  cd $root/stanford-tregex-2012-03-09
+  name=`echo $file | cut -d. -f1 | cut -d/ -f2`
+  ./tsurgeon.sh -treeFile $root/example-sents/$name.txt.head.parsed $root/modality-tagger/modality-patterns/instantiated-templates/$name.txt > $root/output/$name.tree
 done
 
 exit 0
