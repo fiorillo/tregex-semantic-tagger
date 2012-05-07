4e38b3693fa73d74ca38ea66d40e601c3503cb37
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-07 15:14:50 -0400

    put proper tsurgeon command in; hope it works

--- script/test-all.sh
+++ script/test-all.sh
@@ -2,8 +2,7 @@
 
 ls example-sents/*.parsed | while read file; do
   root=`echo $file | cut -d. -f1 | cut -d/ -f2`
-  echo $root
-  #stanford-tregex-2012-03-09/tsurgeon.sh -treeFile $file 
+  stanford-tregex-2012-03-09/tsurgeon.sh -treeFile $file modality-tagger/modality-patterns/instantiated-templates/$root.txt > output/$root.tree
 done
 
 exit 0
