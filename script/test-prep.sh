5c674da7ab3620df4bdaae8e88ccba5766e566c1
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-14 13:53:44 -0400

    touched up, added comments

--- script/test-prep.sh
+++ script/test-prep.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# tests all the preprocessing patterns, in the order defined in idiosyncratic/command.txt
+# if you want to change the order or add a new pattern, put it there (or come up with a better solution)
+# uses the sentences in modality-tagger/test/prep.txt.parsed to test.
+# any file parsed with the stanford parser will do though.
+
 # root directory of project file; change this if need be
 root=/Users/matthewfiorillo/Documents/school/tsurgeon
 cd $root
 
 # load up order of preprocessing and other idiosyncratic patterns
-# shell is FUCKED right now, so i just put everything on one line...
 order=`cat modality-tagger/modality-patterns/idiosyncratic/command.txt | sed -e 's/ / \/Users\/matthewfiorillo\/Documents\/school\/tsurgeon\/modality-tagger\/modality-patterns\/idiosyncratic\//g'`
 
 echo $order
