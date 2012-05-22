356da982fee7ddcec3bcf62d398a1e0a11bd2187
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-22 16:42:31 -0400

    script now actually runs the user-selected pattern (whoops)

--- script/test-pattern.sh
+++ script/test-pattern.sh
@@ -15,7 +15,8 @@ stanford-parser-2012-01-06/lexparser.sh $tmp_dir/$pattern.txt > $tmp_dir/$patter
 
 cd $root/stanford-tregex-2012-03-09
 ./tsurgeon.sh -treeFile $tmp_dir/$pattern.parsed
-  $prep_dir/relabel-vb-zero.txt $prep_dir/remove-g.txt $prep_dir/remove-n.txt $prep_dir/remove-d.txt $prep_dir/remove-z.txt $prep_dir/remove-p.txt $prep_dir/mark-have-aux.txt $prep_dir/mark-be-aux.txt $prep_dir/mark-get-passive.txt $prep_dir/mark-passive-verbs.txt $prep_dir/md-must-have.txt $prep_dir/negative.txt $prep_dir/negative-never.txt $prep_dir/negative-finite-be.txt $prep_dir/have-to.txt $prep_dir/let-vp.txt $prep_dir/let-s.txt $prep_dir/have-need-of.txt $prep_dir/in-need-of.txt $prep_dir/fall-short.txt $prep_dir/fall-short-jj.txt
+  $prep_dir/relabel-vb-zero.txt $prep_dir/remove-g.txt $prep_dir/remove-n.txt $prep_dir/remove-d.txt $prep_dir/remove-z.txt $prep_dir/remove-p.txt $prep_dir/mark-have-aux.txt $prep_dir/mark-be-aux.txt $prep_dir/mark-get-passive.txt $prep_dir/mark-passive-verbs.txt $prep_dir/md-must-have.txt $prep_dir/negative.txt $prep_dir/negative-never.txt $prep_dir/negative-finite-be.txt $prep_dir/have-to.txt $prep_dir/let-vp.txt $prep_dir/let-s.txt $prep_dir/have-need-of.txt $prep_dir/in-need-of.txt $prep_dir/fall-short.txt $prep_dir/fall-short-jj.txt \
+  $pattern_dir/$pattern.txt
 
 rm $tmp_dir/$pattern*
 
