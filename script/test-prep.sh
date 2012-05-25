c20dbb648a17635983adf3cadc952dbef6186a5d
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-25 17:07:32 -0400

    fixed filename error

--- script/test-prep.sh
+++ script/test-prep.sh
@@ -17,7 +17,7 @@ $parser_dir/lexparser.sh $tmp_dir/prep.txt > $tmp_dir/prep.parsed
 
 # tag the sentence
 cd $tregex_dir
-./tsurgeon.sh -treeFile $tmp_dir/$pattern.parsed \
+./tsurgeon.sh -treeFile $tmp_dir/prep.parsed \
   $prep_dir/relabel-vb-zero.txt $prep_dir/remove-g.txt $prep_dir/remove-n.txt $prep_dir/remove-d.txt $prep_dir/remove-z.txt $prep_dir/remove-p.txt $prep_dir/mark-have-aux.txt $prep_dir/mark-be-aux.txt $prep_dir/mark-get-passive.txt $prep_dir/mark-passive-verbs.txt $prep_dir/md-must-have.txt $prep_dir/negative.txt $prep_dir/negative-never.txt $prep_dir/negative-finite-be.txt $prep_dir/have-to.txt $prep_dir/let-vp.txt $prep_dir/let-s.txt $prep_dir/have-need-of.txt $prep_dir/in-need-of.txt $prep_dir/fall-short.txt $prep_dir/fall-short-jj.txt \
 
 rm $tmp_dir/prep*
