442b3945f77840b298c0234ea34dc60a517cf6bc
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-25 17:05:56 -0400

    fixed overzealous tmp file removal

--- script/test-prep.sh
+++ script/test-prep.sh
@@ -12,15 +12,14 @@ parser_dir=$PROJECT_ROOT/tools/stanford-parser
 tregex_dir=$PROJECT_ROOT/tools/stanford-tregex
 
 # parse the sentence
-echo $sentence > $tmp_dir/$pattern.txt
-$parser_dir/lexparser.sh $tmp_dir/$pattern.txt > $tmp_dir/$pattern.parsed
+echo $sentence > $tmp_dir/prep.txt
+$parser_dir/lexparser.sh $tmp_dir/prep.txt > $tmp_dir/prep.parsed
 
 # tag the sentence
 cd $tregex_dir
 ./tsurgeon.sh -treeFile $tmp_dir/$pattern.parsed \
   $prep_dir/relabel-vb-zero.txt $prep_dir/remove-g.txt $prep_dir/remove-n.txt $prep_dir/remove-d.txt $prep_dir/remove-z.txt $prep_dir/remove-p.txt $prep_dir/mark-have-aux.txt $prep_dir/mark-be-aux.txt $prep_dir/mark-get-passive.txt $prep_dir/mark-passive-verbs.txt $prep_dir/md-must-have.txt $prep_dir/negative.txt $prep_dir/negative-never.txt $prep_dir/negative-finite-be.txt $prep_dir/have-to.txt $prep_dir/let-vp.txt $prep_dir/let-s.txt $prep_dir/have-need-of.txt $prep_dir/in-need-of.txt $prep_dir/fall-short.txt $prep_dir/fall-short-jj.txt \
 
-# cleanup
-rm $tmp_dir/$pattern*
+rm $tmp_dir/prep*
 
 exit 0
