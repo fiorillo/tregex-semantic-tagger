0f3553b9972992c771bc65657116519a4b375e3b
Matthew Fiorillo <fiorillo@cmu.edu>
2012-05-29 22:32:50 -0400

    changed from literal list of tsurgeon scripts to run

--- script/test-word.sh
+++ script/test-word.sh
@@ -18,9 +18,7 @@ $parser_dir/lexparser.sh $tmp_dir/$word.txt > $tmp_dir/$word.parsed
 
 # tag the sentence
 cd $tregex_dir
-./tsurgeon.sh -treeFile $tmp_dir/$word.parsed \
-  $prep_dir/relabel-vb-zero.txt $prep_dir/remove-g.txt $prep_dir/remove-n.txt $prep_dir/remove-d.txt $prep_dir/remove-z.txt $prep_dir/remove-p.txt $prep_dir/mark-have-aux.txt $prep_dir/mark-be-aux.txt $prep_dir/mark-get-passive.txt $prep_dir/mark-passive-verbs.txt $prep_dir/md-must-have.txt $prep_dir/negative.txt $prep_dir/negative-never.txt $prep_dir/negative-finite-be.txt $prep_dir/have-to.txt $prep_dir/let-vp.txt $prep_dir/let-s.txt $prep_dir/have-need-of.txt $prep_dir/in-need-of.txt $prep_dir/fall-short.txt $prep_dir/fall-short-jj.txt \
-  $pattern_dir/$word*
+./tsurgeon.sh -treeFile $tmp_dir/$word.parsed $prep_dir/*.txt $pattern_dir/$word*
 
 # cleanup
 rm $tmp_dir/$word*
