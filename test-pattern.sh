2cc41cc008f1110b32cccd6f4c2552268eeb91ba
Matthew Fiorillo <fiorillo@cmu.edu>
2013-02-20 15:30:38 -0500

    fixed pattern path again, should work this time

--- test-pattern.sh
+++ test-pattern.sh
@@ -3,15 +3,16 @@
 # runs all the patterns on the given parsed file, and prints the modified parse tree to stdout
 # usage: tag.sh [pattern-to-test] [parsed-text-file] > [output file]
 
+pattern=$1
+treefile=$2
+
 root=`pwd`
-pattern=$root/$1
-treefile=$root/$2
 prep_dir=$root/modality-patterns/preparatory
 cleanup_dir=$root/modality-patterns/cleanup
 tregex_dir=$root/tools/stanford-tregex
 
 # run the patterns, in order
 cd $tregex_dir
-./tsurgeon.sh -treeFile $root/$treefile $prep_dir/*.txt $pattern $cleanup_dir/*.txt
+./tsurgeon.sh -treeFile $root/$treefile $prep_dir/*.txt $root/$pattern $cleanup_dir/*.txt
 
 exit 0
