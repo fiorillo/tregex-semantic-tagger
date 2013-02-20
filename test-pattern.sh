9573c2ec3391c588a23505d54204436d0cd8953f
Matthew Fiorillo <fiorillo@cmu.edu>
2013-02-20 15:22:13 -0500

    added pattern testing script

---
+++ test-pattern.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# runs all the patterns on the given parsed file, and prints the modified parse tree to stdout
+# usage: tag.sh [pattern-to-test] [parsed-text-file] > [output file]
+
+pattern=$1
+treefile=$2
+
+root=`pwd`
+prep_dir=$root/modality-patterns/preparatory
+cleanup_dir=$root/modality-patterns/cleanup
+tregex_dir=$root/tools/stanford-tregex
+
+# run the patterns, in order
+cd $tregex_dir
+./tsurgeon.sh -treeFile $root/$treefile $prep_dir/*.txt $pattern $cleanup_dir/*.txt
+
+exit 0
